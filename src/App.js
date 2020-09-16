7c2f35d5f5742d6fbf37a2c6e7d812647fe3ee2f
hemsleek <asiyanbimubashir@gmail.com>
2020-09-16 10:08:52 +0100

    feat:main content and Add Todo

--- src/App.js
+++ src/App.js
@@ -11,17 +11,20 @@ const Todos=() =>{
     // }
     return(
     <div className="Todos">
-        <h2>
-          Title   
-        </h2>
+        <p>
+        How to withdraw Aishat Money and with been unaware
+        </p>
+        <span className="Details"> 
+
+        </span>
         
    </div>
     )
 }
-
+// Todos
 const SearchBox =() =>(
     <div className="SearchBox">
-        <img src="/vectors/search.png" alt="search-icon"/>
+        <img src="/vectors/search.svg" alt="search-icon"/>
         <input type="text" />
     </div>
 )
@@ -38,18 +41,34 @@ const TodoWrapper = () =>(
         
     </div>
 )
+// MAin-content
 const AddTodo = () =>(
     <div className="AddTodo">
-        <input type="text" placeholder="" />
-        <textarea ></textarea>
+        <input type="text" placeholder="Title" />
+        <textarea placeholder="Type your note"></textarea>
+
+        <div className="DesignSubmit">
+
+            <div className="Colors">
+                <img src="/vectors/teal.svg" alt="teal" />
+                <img src="/vectors/purple.svg" alt="yellow" />
+                <img src="/vectors/yellow.svg" alt="yellow" />
+            </div>
+            <img src="/vectors/tick.svg" alt="tick" />
+        </div>
     </div>
 )
 
 const MainContent =() =>(
-    <div  className="MainConent">
+    <div  className="MainContent">
         <AddTodo />
-        <div>
-            
+        <div className="Screen">
+            <h3 className="Title">
+                How to withdraw Aishat Money with her been unaware
+            </h3>
+            <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ratione id explicabo distinctio quaerat non? Necessitatibus enim vel, ducimus perspiciatis debitis provident alias, aperiam aliquam placeat cumque tempora maxime dignissimos ipsa.
+            </p>
         </div>
     </div>
 )
